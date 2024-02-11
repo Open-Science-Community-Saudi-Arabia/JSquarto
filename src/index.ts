bad38eea1ad2b7ff802cac0eb816d6cf4c201f52
RealRichi3 <molunorichie@gmail.com>
2024-02-11 08:08:38 +0100

    clean code

--- src/index.ts
+++ src/index.ts
@@ -1,162 +1,128 @@
-import fs from 'fs'
-import { Doc, ModuleBlockInfo } from './interfaces'
-import { CommentsUtil } from './utils/comment'
-import SourceFile from './utils/file'
-import Writer from './utils/writer'
-import { Category, Module, ModuleDoc, SubCategory } from './utils/docStructureGenerator'
+import fs from 'fs';
+import Comment, { CommentsUtil } from './utils/comment';
+import SourceFile from './utils/file';
+import Writer from './utils/writer';
+import { Category, Module, ModuleDoc, SubCategory } from './utils/docStructureGenerator';
+import logger from "./utils/logger";
 
 function getJSFilesFromDirectory(directory: string): string[] {
-    // Get all nested files and folders
-    const files = fs.readdirSync(directory)
-    const allFiles: string[] = []
-    for (const file of files) {
-        const filePath = directory + '/' + file
-        const isDirectory = fs.statSync(filePath).isDirectory()
-
-        if (isDirectory) {
-            const filesInDirectory = getJSFilesFromDirectory(filePath)
-            allFiles.push(...filesInDirectory)
-        } else {
-            allFiles.push(filePath)
+    const files: string[] = [];
+    const items = fs.readdirSync(directory);
+    for (const item of items) {
+        const itemPath = `${directory}/${item}`;
+        if (fs.statSync(itemPath).isDirectory()) {
+            files.push(...getJSFilesFromDirectory(itemPath));
+        } else if (itemPath.endsWith('.js')) {
+            files.push(itemPath);
         }
     }
+    return files;
+}
 
-    return allFiles.filter((file) => file.includes('.js'))
+function createOrUpdateModule(name: string, description: string | undefined, category: string | undefined, modules: Map<string, Module>, categories: Map<string, Category>): Module {
+    let newModule = modules.get(name);
+    if (!newModule) {
+        newModule = new Module({ name, description, category });
+        modules.set(name, newModule);
+        logger.info(`Module '${name}' created.`);
+    } else {
+        logger.info(`Module '${name}' already exists.`);
+    }
+    if (category) {
+        let cat = categories.get(category);
+        if (!cat) {
+            cat = new Category(category);
+            categories.set(category, cat);
+            logger.info(`Category '${category}' created.`);
+        }
+    }
+    return newModule;
+}
+
+function addSubCategory(category: Category, subCategoryName: string) {
+    const subCategory = category.subCategories.find(subCat => subCat.name === subCategoryName);
+    if (!subCategory) {
+        const newSubCategory = new SubCategory({ name: subCategoryName, category });
+        category.subCategories.push(newSubCategory);
+        logger.info(`Sub-category '${subCategoryName}' added to category '${category.name}'.`);
+    }
+}
+
+function processComments(comments: Comment[], modules: Map<string, Module>, categories: Map<string, Category>, defaultCategory: Category, defaultSubCategory: SubCategory, defaultFileModule: Module, filePath: string) {
+    let fileModule: Module | undefined = undefined;
+    const moduleDocs: ModuleDoc[] = [];
+
+    for (const comment of comments) {
+        if (comment.blockInfo.type !== 'module') {
+            moduleDocs.push(new ModuleDoc({ originalFilePath: filePath, data: comment.getOtherBlockInfo() }));
+            continue;
+        }
+
+        const _module = comment.getModuleInfo();
+        fileModule = createOrUpdateModule(_module.name, _module.description, _module.category?.name, modules, categories);
+
+        if (!_module.category) continue;
+
+        addSubCategory(categories.get(_module.category.name) || defaultCategory, _module.category.subCategory);
+    }
+
+    if (fileModule) {
+        moduleDocs.forEach(doc => fileModule.addDoc(doc));
+        const category = fileModule.info.category?.name || defaultCategory.name;
+        const subCategory = fileModule.info.category?.subCategory || defaultSubCategory.name;
+        const categoryToAddTo = categories.get(category);
+        const subCategoryToAddTo = categoryToAddTo?.subCategories.find(subCat => subCat.name === subCategory);
+
+        if (subCategoryToAddTo) {
+            if (!subCategoryToAddTo.getModules().some(module => module.info.name === fileModule.info.name)) {
+                subCategoryToAddTo.addModule(fileModule);
+            }
+        } else if (categoryToAddTo) {
+            if (!categoryToAddTo.getModules().some(module => module.info.name === fileModule.info.name)) {
+                categoryToAddTo.addModule(fileModule);
+            }
+        } else {
+            if (!defaultCategory.getModules().some(module => module.info.name === fileModule.info.name)) {
+                defaultSubCategory.addModule(fileModule);
+            }
+        }
+
+        modules.set(fileModule.info.name, fileModule);
+    } else {
+        moduleDocs.forEach(doc => defaultFileModule.addDoc(doc));
+    }
 }
 
 function start() {
-    // Get all folders and files in test_files directory that are js files
-    const filePaths = getJSFilesFromDirectory(__dirname + '/../test_files')
-
-    const docs: {
-        module: ModuleBlockInfo,
-        originalFilePath: string,
-        docs: Doc[]
-    }[] = []
-
-    const modules: Module[] = []
+    const filePaths = getJSFilesFromDirectory(__dirname + '/../test_files');
+    const modules: Map<string, Module> = new Map();
+    const categories: Map<string, Category> = new Map();
 
     const defaultFileModule = new Module({
         name: 'default',
         description: 'Default module',
         link: 'default'
-    })
+    });
 
-    const categories: Category[] = []
+    const defaultCategory = new Category('default');
+    const defaultSubCategory = new SubCategory({ name: 'default', category: defaultCategory });
+    defaultCategory.subCategories.push(defaultSubCategory);
+    categories.set(defaultCategory.name, defaultCategory);
 
-    const defaultCategory = new Category('default')
-    const defaultSubCategory = new SubCategory({ name: 'default', category: defaultCategory })
-
-    categories.push(defaultCategory)
-    defaultCategory.subCategories.push(defaultSubCategory)
-
-    // Get all comments from all files
     for (const filePath of filePaths) {
-        const sourceFile = new SourceFile(filePath)
-        const comments = CommentsUtil.getCommentsFromFile(sourceFile.fileContent)
-
-        let moduleHasBeenDeclaredForFile = false
-
-        // There should be one module for each file
-        // Get all the comments for each file,
-        // While getting the comments check if the current comment is a module comment
-        // If it is the first module, check the module array if the module has been initialized before
-        // If it has not been initialized, then add it to the array of all modules
-        // Link all the comments in this file to the module
-        // If no module has been declared, then the module is the default module
-
-        let fileModule: Module | undefined = undefined
-
-        const moduleDocs: ModuleDoc[] = []
-
-        for (const comment of comments) {
-            const blockType = comment.blockInfo.type
-            const commentIsModule = blockType === 'module'
-
-            if (!commentIsModule) {
-                // Comment is not a module declaration
-                // Check which module the comment belongs to
-                moduleDocs.push(new ModuleDoc({ originalFilePath: filePath, data: comment.getOtherBlockInfo() }))
-                continue
-            }
-
-            const _module = comment.getModuleInfo()
-            const moduleExists = modules.some((module) => module.info.name === _module.name)
-
-            const newModule = new Module({ name: _module.name, description: _module.description, category: _module.category, })
-            if (!moduleExists) modules.push(newModule)
-
-            if (!moduleHasBeenDeclaredForFile) {
-                fileModule = newModule
-                moduleHasBeenDeclaredForFile = true
-            }
-
-            if (_module.category) {
-                const existingCategory = categories.find((category) => category.name === _module.category?.name)
-
-                if (existingCategory) {
-                    // Check if subcategory has already been added to the main category
-                    const existingSubCategory = existingCategory.subCategories.find((subCategory) => subCategory.name === _module.category?.subCategory)
-
-                    const subCategory = existingSubCategory ?? new SubCategory({ name: _module.category?.subCategory, category: new Category(_module.category?.name) })
-
-                    !existingSubCategory && existingCategory.subCategories.push(subCategory)
-
-                } else {
-                    const newCategory = new Category(_module.category?.name)
-                    const subCategory = new SubCategory({ name: _module.category?.subCategory, category: newCategory })
-                    newCategory.subCategories.push(subCategory)
-                    categories.push(newCategory)
-                }
-            }
-        }
-
-        // Add all the comments to the module
-        if (fileModule) {
-            moduleDocs.forEach((doc) => fileModule.addDoc(doc))
-
-            // Get the category and subcategory for the module
-            // If the module has a category and subcategory, then add the module to the category and subcategory
-            // If the module has a category but no subcategory, then add the module to the category
-            // If the module has no category, then add the module to the default category and subcategory
-            const category = fileModule.info.category?.name ?? defaultCategory.name
-            const subCategory = fileModule.info.category?.subCategory ?? defaultSubCategory.name
-
-            const categoryToAddTo = categories.find((_category) => _category.name === category)
-            const subCategoryToAddTo = categoryToAddTo?.subCategories.find((_subCategory) => _subCategory.name === subCategory)
-
-            if (subCategoryToAddTo) {
-                const existingModule = subCategoryToAddTo.getModules().find((_module) => _module.info.name === fileModule.info.name)
-                if (!existingModule) {
-                    subCategoryToAddTo.addModule(fileModule)
-                }
-            } else if (categoryToAddTo) {
-                const existingModuleInCategory = categoryToAddTo.getModules().find((_module) => _module.info.name === fileModule.info.name)
-                if (!existingModuleInCategory) {
-                    categoryToAddTo.addModule(fileModule)
-                }
-            } else {
-                const existingModuleInDefaultCategory = defaultCategory.getModules().find((_module) => _module.info.name === fileModule.info.name)
-                if (!existingModuleInDefaultCategory) {
-
-                    defaultSubCategory.addModule(fileModule)
-                }
-            }
-
-            modules.push(fileModule)
-        } else {
-            moduleDocs.forEach((doc) => defaultFileModule.addDoc(doc))
-        }
+        const sourceFile = new SourceFile(filePath);
+        const comments = CommentsUtil.getCommentsFromFile(sourceFile.fileContent);
+        processComments(comments, modules, categories, defaultCategory, defaultSubCategory, defaultFileModule, filePath);
     }
 
     if (defaultFileModule.getDocs().length > 0) {
-        modules.push(defaultFileModule)
-        defaultCategory.addModule(defaultFileModule)
+        modules.set(defaultFileModule.info.name, defaultFileModule);
+        defaultCategory.addModule(defaultFileModule);
     }
 
     new Writer()
-        .prepareDirectoryForDocs(categories)
-        .writeDocsFromCategoriesToFile(categories)
+        .prepareDirectoryForDocs(Array.from(categories.values()))
+        .writeDocsFromCategoriesToFile(Array.from(categories.values()));
 }
 
-start()
+start();
--- src/utils/writer.ts
+++ src/utils/writer.ts
@@ -1,11 +1,7 @@
-/**
- * This file contains a Writer class that writes documentation to Markdown files (*.qmd). 
- * The Markdown content based on this information and writes it to the specified location.
- */
-
 import { Doc, ModuleBlockInfo } from "../interfaces";
 import fs from 'fs';
 import { Category, ModuleDoc } from "./docStructureGenerator";
+import logger from "./logger";
 
 export default class Writer {
     private writeDocsToFile({
@@ -14,93 +10,102 @@ export default class Writer {
         docs
     }: { module: ModuleBlockInfo, destinationPath: string, docs: ModuleDoc[] }) {
         // Get file path
-        const qmdfilePath = destinationPath + '/' + module.name + '.qmd'
-        fs.writeFileSync(qmdfilePath, '', 'utf8')
+        const qmdfilePath = destinationPath + '/' + module.name + '.qmd';
 
-        let fileContent = ''
+        try {
+            fs.writeFileSync(qmdfilePath, '', 'utf8');
 
-        //  Add module title to qmd file
-        fileContent += `--- \n title: ${module.name} \n---\n`
+            let fileContent = '';
 
-        // Add module description to qmd file
-        fileContent += `## Description \n ${module.description} \n`
+            //  Add module title to qmd file
+            fileContent += `--- \n title: ${module.name} \n---\n`;
 
-        // Add constructs to qmd file
-        for (const _doc of docs) {
-            const doc = {
-                blockInfo: _doc.data,
-                constructInfo: {
-                    type: 'Function',
-                    name: module.name
+            // Add module description to qmd file
+            fileContent += `## Description \n ${module.description} \n`;
+
+            // Add constructs to qmd file
+            for (const _doc of docs) {
+                const doc = {
+                    blockInfo: _doc.data,
+                    constructInfo: {
+                        type: 'Function',
+                        name: module.name
+                    }
+                };
+
+                // Add 2 lines
+                fileContent += '\n\n';
+
+                fileContent += `## ${doc.constructInfo.type} ${doc.constructInfo.name} \n`;
+
+                // Add description to qmd file
+                fileContent += `### Description \n ${doc.blockInfo.description} \n`;
+
+                // Add params to qmd file
+                if (doc.blockInfo.params.length > 0) {
+                    fileContent += `### Params \n`;
+                    for (const param of doc.blockInfo.params) {
+                        fileContent += `**${param.name}**: ${param.description} \n`;
+                    }
+                }
+
+                // Add returns to qmd file
+                if (doc.blockInfo.returns.length > 0) {
+                    fileContent += `### Returns \n`;
+                    for (const returnedValue of doc.blockInfo.returns) {
+                        fileContent += `**${returnedValue.type}**: ${returnedValue.description} \n`;
+                    }
+                }
+
+                // Add thrown errors to qmd file
+                if (doc.blockInfo.thrownErrors.length > 0) {
+                    fileContent += `### Thrown Errors \n`;
+                    for (const thrownError of doc.blockInfo.thrownErrors) {
+                        fileContent += `**${thrownError.type}**: ${thrownError.description} \n`;
+                    }
+                }
+
+                // Add link to qmd file
+                if (doc.blockInfo.link) {
+                    fileContent += `### Link \n ${doc.blockInfo.link} \n`;
                 }
             }
-            // Add 2 lines
-            fileContent += '\n\n'
 
-            fileContent += `## ${doc.constructInfo.type} ${doc.constructInfo.name} \n`
-
-            // Add description to qmd file
-            fileContent += `### Description \n ${doc.blockInfo.description} \n`
-
-            // Add params to qmd file
-            if (doc.blockInfo.params.length > 0) {
-                fileContent += `### Params \n`
-                for (const param of doc.blockInfo.params) {
-                    fileContent += `**${param.name}**: ${param.description} \n`
-                }
-            }
-
-            // Add returns to qmd file
-            if (doc.blockInfo.returns.length > 0) {
-                fileContent += `### Returns \n`
-                for (const returnedValue of doc.blockInfo.returns) {
-                    fileContent += `**${returnedValue.type}**: ${returnedValue.description} \n`
-                }
-            }
-
-            // Add thrown errors to qmd file
-            if (doc.blockInfo.thrownErrors.length > 0) {
-                fileContent += `### Thrown Errors \n`
-                for (const thrownError of doc.blockInfo.thrownErrors) {
-                    fileContent += `**${thrownError.type}**: ${thrownError.description} \n`
-                }
-            }
-
-            // Add link to qmd file
-            if (doc.blockInfo.link) {
-                fileContent += `### Link \n ${doc.blockInfo.link} \n`
-            }
+            fs.writeFileSync(qmdfilePath, fileContent, 'utf8');
+            logger.info(`Documentation written to file: ${qmdfilePath}`);
+        } catch (error) {
+            logger.error(`Error writing documentation to file: ${qmdfilePath}`);
+            logger.error(error);
         }
-
-        fs.writeFileSync(qmdfilePath, fileContent, 'utf8')
     }
 
     public writeDocsFromCategoriesToFile(categories: Category[]) {
         for (const category of categories) {
-            const categoryFolderPath = __dirname + `/../../docs/${category.name}`
+            const categoryFolderPath = __dirname + `/../../docs/${category.name}`;
 
             for (const subCategory of category.subCategories) {
-                const subCategoryFolderPath = categoryFolderPath + '/' + subCategory.name
+                const subCategoryFolderPath = categoryFolderPath + '/' + subCategory.name;
 
                 for (const module of subCategory.getModules()) {
                     this.writeDocsToFile({
                         module: module.info,
                         destinationPath: subCategoryFolderPath,
                         docs: module.getDocs()
-                    })
+                    });
                 }
             }
         }
 
-        return this
+        return this;
     }
 
     public prepareDirectoryForDocs(categories: Category[]) {
-        const folderPathToWrite = __dirname + `/../../docs`
+        const folderPathToWrite = __dirname + `/../../docs`;
 
         // Create folder if it doesn't exist
         if (!fs.existsSync(folderPathToWrite)) {
-            fs.mkdirSync(folderPathToWrite, { recursive: true })
+            fs.mkdirSync(folderPathToWrite, { recursive: true });
+            logger.info(`Documentation folder created: ${folderPathToWrite}`);
         }
 
         // Create sub folders for each category and add a default index.qmd file for each category
@@ -108,23 +113,27 @@ export default class Writer {
         // Write the category name to the index.qmd file 
         // Write the sub category name to the index.qmd file
         for (const category of categories) {
-            const categoryFolderPath = folderPathToWrite + '/' + category.name
+            const categoryFolderPath = folderPathToWrite + '/' + category.name;
             if (!fs.existsSync(categoryFolderPath)) {
-                fs.mkdirSync(categoryFolderPath, { recursive: true })
+                fs.mkdirSync(categoryFolderPath, { recursive: true });
+                logger.info(`Category folder created: ${categoryFolderPath}`);
             }
 
-            fs.writeFileSync(categoryFolderPath + '/index.qmd', `--- \n title: ${category.name} \n---\n`, 'utf8')
+            fs.writeFileSync(categoryFolderPath + '/index.qmd', `--- \n title: ${category.name} \n---\n`, 'utf8');
+            logger.info(`Index.qmd file created for category: ${category.name}`);
 
             for (const subCategory of category.subCategories) {
-                const subCategoryFolderPath = categoryFolderPath + '/' + subCategory.name
+                const subCategoryFolderPath = categoryFolderPath + '/' + subCategory.name;
                 if (!fs.existsSync(subCategoryFolderPath)) {
-                    fs.mkdirSync(subCategoryFolderPath, { recursive: true })
+                    fs.mkdirSync(subCategoryFolderPath, { recursive: true });
+                    logger.info(`Sub-category folder created: ${subCategoryFolderPath}`);
                 }
 
-                fs.writeFileSync(subCategoryFolderPath + '/index.qmd', `--- \n title: ${subCategory.name} \n---\n`, 'utf8')
+                fs.writeFileSync(subCategoryFolderPath + '/index.qmd', `--- \n title: ${subCategory.name} \n---\n`, 'utf8');
+                logger.info(`Index.qmd file created for sub-category: ${subCategory.name}`);
             }
         }
 
-        return this
+        return this;
     }
 }
