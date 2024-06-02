c91f59f0e179aed83e6453c7a60b1dfee19eb0f9
RealRichi3 <molunorichie@gmail.com>
2024-06-03 00:53:17 +0100

    fix: npm scripts throwing error when extra args are passed

--- src/jsq.ts
+++ src/jsq.ts
@@ -1,11 +1,40 @@
 import { execSync } from "child_process";
 import path from "path";
+import logger from "./utils/logger";
 
+const allowedCommands = [
+    "doc:generate",
+    "doc:preview",
+    "doc:clean",
+    "doc:serve",
+    "fix:all",
+    "fix:crowdin_files",
+    "fix:localized_files",
+    "fix:duplicate_languages_ref",
+    "fix:wrong_languages_ref",
+];
+const commandsWithNoArgs = [
+    "doc:preview",
+    "doc:clean",
+    "setup-quarto",
+    "update-quarto",
+];
+
+// const additionalArgs = args.slice(1).join(" ") + " workingDir=" + process.cwd();
 // Determine the jsq tool directory
 const projectDir = path.resolve(__dirname, "..");
 const args = process.argv.slice(2);
-const additionalArgs = args.slice(1).join(" ") + " workingDir=" + process.cwd();
 const script = args[0];
+const additionalArgs = args.slice(1).join(" ");
+
+if (!allowedCommands.includes(script)) {
+    logger.error(`Invalid command: ${script}`);
+    process.exit(1);
+}
+
+const command = commandsWithNoArgs.includes(script)
+    ? script
+    : `${script} -- ${additionalArgs}`;
 
 process.chdir(projectDir);
-execSync(`npm run ${script} -- ${additionalArgs}`, { stdio: "inherit" });
+execSync(`npm run ${command}`, { stdio: "inherit" });
