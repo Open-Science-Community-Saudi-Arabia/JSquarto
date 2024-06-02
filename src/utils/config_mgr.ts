1a3a9d5c74c78ff2bf40e0490fa0e56fffe3102c
RealRichi3 <molunorichie@gmail.com>
2024-06-03 00:16:19 +0100

    refactor code

--- src/utils/config_mgr.ts
+++ src/utils/config_mgr.ts
@@ -67,15 +67,6 @@ export default class ConfigMgr {
             const arg = args[i];
             let [key, value] = arg.split("=");
 
-            if (
-                key === "source" ||
-                key === "output" ||
-                key === "tutorial" ||
-                key === "translations"
-            ) {
-                value = `${this.currentWorkingDirectory}/${value}`;
-            }
-
             argMap.set(key.startsWith("--") ? key.slice(2) : key, value);
         }
 
@@ -88,7 +79,7 @@ export default class ConfigMgr {
         const currentConfig = this.CONFIG;
         const configToUpdate = {} as Config;
         for (const entries of cliArgs.entries()) {
-            const [cliKey, cliValue] = entries as [
+            let [cliKey, cliValue] = entries as [
                 keyof CliArgs,
                 ValueOf<CliArgs>,
             ];
@@ -114,10 +105,19 @@ export default class ConfigMgr {
                 newData: configToUpdate,
             },
         });
-        for (const [key, value] of Object.entries(updatedConfig)) {
+        for (let [key, value] of Object.entries(updatedConfig)) {
             if (key === "languages") {
                 this.CONFIG.languages = value as string[];
             } else {
+                if (
+                    key === "sourceDirectory" ||
+                    key === "outputDirectory" ||
+                    key === "tutorialDirectory" ||
+                    key === "translationsDirectory"
+                ) {
+                    value = `${this.currentWorkingDirectory}/${value}`;
+                }
+
                 this.CONFIG[
                     key as
                         | "outputDirectory"
