d6799356db66ca04d63e317403c6842f91153e0b
RealRichi3 <molunorichie@gmail.com>
2024-06-08 16:37:40 +0100

    update logger forma

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -16,12 +16,28 @@
  * */
 
 import winston from "winston";
+import util from "util";
+
+const logFormat = winston.format.printf((info) => {
+    let message = info.message;
+
+    if (typeof message === "object") {
+        message = util.inspect(message, { depth: null });
+    }
+
+    const logMeta = info.meta ? util.inspect(info.meta, { depth: null }) : "";
+    return info.meta
+        ? `[${info.timestamp}] | ${info.level}: ${message} ${logMeta}`
+        : `[${info.timestamp}] | ${info.level}: ${message}`;
+});
 
 const logger = winston.createLogger({
     level: "info",
     format: winston.format.combine(
         winston.format.colorize(),
+        winston.format.timestamp(),
         winston.format.simple(),
+        logFormat,
     ),
     transports: [new winston.transports.Console()],
 });
